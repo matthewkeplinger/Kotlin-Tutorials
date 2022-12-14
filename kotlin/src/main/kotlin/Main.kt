f9e2efc1831bfd5a39289be7597b6ba46e9e1445
Matthew Keplinger <matthew.keplinger85@gmail.com>
2022-12-14 11:58:32 -0500

    Lesson 1: Conclusion

--- kotlin/src/main/kotlin/Main.kt
+++ kotlin/src/main/kotlin/Main.kt
@@ -1,5 +1,14 @@
+/* Learn Kotlin in 14 hours
+* Matt Keplinger
+* December 14, 2022
+* Kotlin Variables and Declarations; Var vs Val; type mismatches
+*  */
+
 fun main() {
-    var userName : String = "Matt"
-    userName = "Jim"
-    println("This is $userName ")
+    var userName = "Matt"
+    userName = "Mark"
+    var age= 37
+    age = 51
+
+    println("I am $userName and I am $age years old")
 }
